a84d81e11ffaaaefbf303f1f62d21620b7197961
awonderlnd <sadovskaya.alisa@gmail.com>
2024-04-15 20:08:50 +0300

    Create project

---
+++ src/main/java/ru/sadovskaya/springcourse/service/BooksService.java
@@ -0,0 +1,93 @@
+package ru.sadovskaya.springcourse.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.sadovskaya.springcourse.models.Book;
+import ru.sadovskaya.springcourse.models.Person;
+import ru.sadovskaya.springcourse.repository.BooksRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class BooksService {
+
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Book> findAll(boolean sortByYear) {
+        if (sortByYear)
+            return booksRepository.findAll(Sort.by("year"));
+        else
+            return booksRepository.findAll();
+    }
+
+    public List<Book> findWithPagination(Integer page, Integer booksPerPage, boolean sortByYear) {
+        if (sortByYear)
+            return booksRepository.findAll(PageRequest.of(page, booksPerPage, Sort.by("year"))).getContent();
+        else
+            return booksRepository.findAll(PageRequest.of(page, booksPerPage)).getContent();
+    }
+
+    public Book findOne(int id) {
+        Optional<Book> foundBook = booksRepository.findById(id);
+        return foundBook.orElse(null);
+    }
+
+    public List<Book> searchByTitle(String title) {
+        return booksRepository.findByTitleStartingWith(title);
+    }
+
+    @Transactional
+    public void save(Book book) {
+        booksRepository.save(book);
+    }
+
+    @Transactional
+    public void update(int id, Book updatedBook) {
+        Book bookToBeUpdated = booksRepository.findById(id).get();
+        updatedBook.setId(id);
+        updatedBook.setOwner(bookToBeUpdated.getOwner());
+        //updatedBook.setTakenAt(bookToBeUpdated.getTakenAt());
+        booksRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        booksRepository.deleteById(id);
+    }
+
+    public Person getBookOwner(int id) {
+        return booksRepository.findById(id).map(Book::getOwner).orElse(null);
+    }
+
+    //когда человек возвращает книгу
+    @Transactional
+    public void release(int id) {
+        booksRepository.findById(id).ifPresent(
+                book -> {
+                    book.setOwner(null);
+                    //book.setTakenAt(null);
+                });
+    }
+
+    //когда человек забирает книгу
+    @Transactional
+    public void assign(int id, Person selectedPerson) {
+        booksRepository.findById(id).ifPresent(
+                book -> {
+                    book.setOwner(selectedPerson);
+                    //book.setTakenAt(new Date()); // текущее время
+                }
+        );
+    }
+}
---
+++ src/main/java/ru/sadovskaya/springcourse/util/PersonValidator.java
@@ -0,0 +1,30 @@
+package ru.sadovskaya.springcourse.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.sadovskaya.springcourse.models.Person;
+import ru.sadovskaya.springcourse.service.PeopleService;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if (peopleService.getPersonByFullName(person.getFullName()).isPresent())
+            errors.rejectValue("fullName", "", "Человек с таким ФИО уже существует");
+    }
+}
